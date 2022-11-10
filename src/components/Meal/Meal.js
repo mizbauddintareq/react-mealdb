e028bb173c20d2f4257eafc8989c64e7038df3ce
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-11-10 08:03:55 +0600

    add meals container with functionality

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,7 +1,39 @@
-const Meal = ({ data }) => {
+const Meal = ({ data, handleAddToCart }) => {
+  const { strMeal, strArea, strTags, strMealThumb } = data;
+  const info = {
+    strMeal,
+    strArea,
+    quantity: 1,
+  };
   return (
-    <div>
-      <h1>This is meals</h1>
+    <div className="card h-100 bg-base-100 shadow-xl">
+      <figure>
+        <img src={strMealThumb} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {strMeal}
+          <div className="badge badge-secondary">
+            {strArea ? strMeal : "Not Found"}
+          </div>
+        </h2>
+        <p>
+          {strTags ? (
+            strTags
+          ) : (
+            <span className="text-red-600">Tags Not Found</span>
+          )}
+        </p>
+        <div className="card-actions justify-end">
+          <div
+            onClick={() => handleAddToCart(info)}
+            className="badge badge-outline cursor-pointer"
+          >
+            Add To Cart
+          </div>
+          <div className="badge badge-outline cursor-pointer">See Details</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -3,24 +3,40 @@ import Meal from "../Meal/Meal";
 
 const Meals = ({ searchText }) => {
   const [meals, setMeals] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
       .then((res) => res.json())
       .then((data) => setMeals(data?.meals));
   }, [searchText]);
+
+  const handleAddToCart = (cartData) => {
+    const newCartData = [...cart, cartData];
+    setCart(newCartData);
+  };
+  console.log(cart);
   return (
-    <div class="flex flex-row mt-10 w-11/12 mx-auto">
-      <div class="basis-3/4">
-        <div class="grid grid-cols-3 gap-4">
-          {meals.map((meal) => (
-            <Meal key={meal.idMeal} data={meal} />
-          ))}
+    <div>
+      {meals && (
+        <div className="flex flex-row mt-10 w-11/12 mx-auto">
+          <div className="basis-3/4">
+            <div className="grid grid-cols-3 gap-4">
+              {meals.map((meal) => (
+                <Meal
+                  key={meal.idMeal}
+                  data={meal}
+                  handleAddToCart={handleAddToCart}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="basis-1/4">
+            <h1>Cart container</h1>
+          </div>
         </div>
-      </div>
-      <div class="basis-1/4">
-        <h1>Cart container</h1>
-      </div>
+      )}
+      {!meals && <h1>Data Not found</h1>}
     </div>
   );
 };
