d2914a47935cbe1fe1f22a988ce8954aa682be3e
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-11-10 07:34:31 +0600

    add navbar

---
+++ src/components/Meal/Meal.js
@@ -0,0 +1,9 @@
+const Meal = ({ data }) => {
+  return (
+    <div>
+      <h1>This is meals</h1>
+    </div>
+  );
+};
+
+export default Meal;
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Meal from "../Meal/Meal";
 
 const Meals = ({ searchText }) => {
-  console.log(searchText);
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
+      .then((res) => res.json())
+      .then((data) => setMeals(data?.meals));
+  }, [searchText]);
   return (
-    <div>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate fuga
-        illum magni esse ad enim ullam consequuntur. Impedit, consectetur.
-        Corporis.
-      </h1>
+    <div class="flex flex-row mt-10 w-11/12 mx-auto">
+      <div class="basis-3/4">
+        <div class="grid grid-cols-3 gap-4">
+          {meals.map((meal) => (
+            <Meal key={meal.idMeal} data={meal} />
+          ))}
+        </div>
+      </div>
+      <div class="basis-1/4">
+        <h1>Cart container</h1>
+      </div>
     </div>
   );
 };
